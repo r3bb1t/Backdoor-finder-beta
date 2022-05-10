845eb974178fcc7c1806be0983e0418c7e2b0159
r3bb1t <49586603+r3bb1t@users.noreply.github.com>
2022-05-10 17:23:14 +0300

    initial commit

---
+++ Backdoor finder beta/Core.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace Backdoor_finder_beta
+{
+    class Core
+    {
+
+        static public void ListProcesses()
+        {
+            Process[] processCollection = Process.GetProcesses();
+            foreach (Process p in processCollection)
+            {
+                Console.WriteLine("___ " + p.ProcessName + " ___");
+                show_modules(p);
+                Console.WriteLine("\n");
+            }
+        }
+
+        static void show_modules(Process p)
+        {
+            try
+            {
+                foreach (ProcessModule module in p.Modules)
+                {
+                    Console.WriteLine(module.FileName);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An Exception occured while trying to enumerate modules");
+            }
+        }
+    }
+}
--- Backdoor finder beta/Program.cs
+++ Backdoor finder beta/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace Backdoor_finder_beta
+{
+    class Program
+    {
+        static void Main()
+        {
+            Core.ListProcesses();
+        }
+    }
+}

c2de03828073962fbb834af7ee1c5ab1afe25899
r3bb1t <49586603+r3bb1t@users.noreply.github.com>
2022-05-11 21:32:15 +0300

    New commit

--- Backdoor finder beta/Core.cs
+++ Backdoor finder beta/Core.cs
@@ -4,22 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-
+using Security.WinTrust;
 
 namespace Backdoor_finder_beta
 {
     class Core
     {
 
-        static public void ListProcesses()
+        static public void FindUnsigned()
         {
-            Process[] processCollection = Process.GetProcesses();
-            foreach (Process p in processCollection)
-            {
-                Console.WriteLine("___ " + p.ProcessName + " ___");
-                show_modules(p);
-                Console.WriteLine("\n");
-            }
+
+            Process[] proclist = Process.GetProcesses();
+
+                foreach(Process process in proclist)
+                {
+                Console.WriteLine("___ " + process.ProcessName + " ___");
+                    try 
+                    {
+                        foreach (ProcessModule module in process.Modules)
+                        {
+
+                            if (!WinTrust.VerifyEmbeddedSignature(module.FileName))
+                            {
+                                Console.WriteLine(module.FileName);
+                            }
+                        }
+                    }
+                catch (Exception e) 
+                {
+                    Console.WriteLine("The following exception has occured:");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("\n");
+                }
+                   
+                }
         }
 
         static void show_modules(Process p)
--- Backdoor finder beta/Program.cs
+++ Backdoor finder beta/Program.cs
@@ -4,15 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using Security.WinTrust;
 
 
 namespace Backdoor_finder_beta
 {
     class Program
     {
+
+        static void usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("1 - List all unsigned processes");
+        }
+
         static void Main()
         {
-            Core.ListProcesses();
+            int choice;
+
+            usage();
+            choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(choice);
+
+            switch (choice)
+            {
+                case 1:
+                    Core.FindUnsigned();
+                    break;
+            }
+
         }
     }
 }
